8bc761b92b4d404c5bb416c2ef9c6de33d9cc44f
arlehht <105451177+arlehht@users.noreply.github.com>
2022-07-01 17:27:43 -0500

    Entity framework ya está integrado (CRUD), se generaron vistas, controladores, se mapeó la bd, etc.

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaRequisiciones_WebApp;
+
+namespace SistemaRequisiciones_WebApp.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private dbRequisicionesEntities db = new dbRequisicionesEntities();
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productos = db.Productos.Include(p => p.Requisiciones);
+            return View(productos.ToList());
+        }
+
+        // GET: Productos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
+        }
+
+        // GET: Productos/Create
+        public ActionResult Create()
+        {
+            ViewBag.Part = new SelectList(db.Requisiciones, "NoRequisicion", "Area");
+            return View();
+        }
+
+        // POST: Productos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Part,Cantidad,Unidad,Descripcion,Surtido,NoRequisicion")] Productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Part = new SelectList(db.Requisiciones, "NoRequisicion", "Area", productos.Part);
+            return View(productos);
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Part = new SelectList(db.Requisiciones, "NoRequisicion", "Area", productos.Part);
+            return View(productos);
+        }
+
+        // POST: Productos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Part,Cantidad,Unidad,Descripcion,Surtido,NoRequisicion")] Productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Part = new SelectList(db.Requisiciones, "NoRequisicion", "Area", productos.Part);
+            return View(productos);
+        }
+
+        // GET: Productos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
+        }
+
+        // POST: Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Productos productos = db.Productos.Find(id);
+            db.Productos.Remove(productos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/RequisicionesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaRequisiciones_WebApp;
+
+namespace SistemaRequisiciones_WebApp.Controllers
+{
+    public class RequisicionesController : Controller
+    {
+        private dbRequisicionesEntities db = new dbRequisicionesEntities();
+
+        // GET: Requisiciones
+        public ActionResult Index()
+        {
+            var requisiciones = db.Requisiciones.Include(r => r.Productos);
+            return View(requisiciones.ToList());
+        }
+
+        // GET: Requisiciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisiciones requisiciones = db.Requisiciones.Find(id);
+            if (requisiciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(requisiciones);
+        }
+
+        // GET: Requisiciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.NoRequisicion = new SelectList(db.Productos, "Part", "Descripcion");
+            return View();
+        }
+
+        // POST: Requisiciones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NoRequisicion,Fecha,Area,Tipo,EntregarEn,Prioridad,FechaSurtido,CompraServicio")] Requisiciones requisiciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Requisiciones.Add(requisiciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.NoRequisicion = new SelectList(db.Productos, "Part", "Descripcion", requisiciones.NoRequisicion);
+            return View(requisiciones);
+        }
+
+        // GET: Requisiciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisiciones requisiciones = db.Requisiciones.Find(id);
+            if (requisiciones == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.NoRequisicion = new SelectList(db.Productos, "Part", "Descripcion", requisiciones.NoRequisicion);
+            return View(requisiciones);
+        }
+
+        // POST: Requisiciones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NoRequisicion,Fecha,Area,Tipo,EntregarEn,Prioridad,FechaSurtido,CompraServicio")] Requisiciones requisiciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(requisiciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.NoRequisicion = new SelectList(db.Productos, "Part", "Descripcion", requisiciones.NoRequisicion);
+            return View(requisiciones);
+        }
+
+        // GET: Requisiciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisiciones requisiciones = db.Requisiciones.Find(id);
+            if (requisiciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(requisiciones);
+        }
+
+        // POST: Requisiciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Requisiciones requisiciones = db.Requisiciones.Find(id);
+            db.Requisiciones.Remove(requisiciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
